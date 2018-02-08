776bfb74c50467bdebbbe4265a1cd10c21659655
fierylove <zhihao_tan1999@hotmail.com>
2018-02-08 23:47:00 +0800

    editted the images
    ....

--- Assignment2/Base/Source/SceneText.cpp
+++ Assignment2/Base/Source/SceneText.cpp
@@ -124,8 +124,8 @@ void SceneText::Init()
 	MeshBuilder::GetInstance()->GetMesh("housebackwall")->textureID = LoadTGA(lua->getStringValue(lua->theImageState, "houselow"));
 	MeshBuilder::GetInstance()->GenerateOBJ("houserightwall", lua->getStringValue(lua->theObjectState, "houserightwall"));
 	MeshBuilder::GetInstance()->GetMesh("houserightwall")->textureID = LoadTGA(lua->getStringValue(lua->theImageState, "houselow"));
-	MeshBuilder::GetInstance()->GenerateOBJ("housefrontwall", "OBJ//LOD//housefrontwall.obj");
-	MeshBuilder::GetInstance()->GetMesh("housefrontwall")->textureID = LoadTGA("Image//LOD//househigh.tga");
+	//MeshBuilder::GetInstance()->GenerateOBJ("housefrontwall", "OBJ//LOD//housefrontwall.obj");
+	//MeshBuilder::GetInstance()->GetMesh("housefrontwall")->textureID = LoadTGA("Image//LOD//househigh.tga");
 	MeshBuilder::GetInstance()->GenerateOBJ("houselowdoor", lua->getStringValue(lua->theObjectState, "housedoor"));
 	MeshBuilder::GetInstance()->GetMesh("houselowdoor")->textureID = LoadTGA(lua->getStringValue(lua->theImageState, "houselow"));
 	MeshBuilder::GetInstance()->GenerateOBJ("housemeddoor", lua->getStringValue(lua->theObjectState, "housedoor"));
@@ -237,10 +237,14 @@ void SceneText::Init()
 
 	//gameplay
 	gameEnd = false;
+	timerToWinGame = 0;
 }
 
 void SceneText::Update(double dt)
 {
+	timerToWinGame += dt;
+	if (timerToWinGame > 30.f)
+		gameEnd = true;
 	
 		// Update our entities
 		EntityManager::GetInstance()->Update(dt);
